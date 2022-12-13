17b8b1c911c0dd464e0b6cfc9cec7903e664c836
Кирилл <RillGd@gmail.com>
2022-12-13 22:04:45 +0200

    Initial commit

---
+++ src/main/java/com/kirillzhdanov/learningrestapi/LearningRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.kirillzhdanov.learningrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kirillzhdanov/learningrestapi/LearningRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirillzhdanov.learningrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
